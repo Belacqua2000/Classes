1c1639180d50a38f7e0ddf4a4ad8ba8dccf8216e
Nick Baughan <nicholas.baughan@hotmail.co.uk>
2020-09-05 08:06:53 +0100

    Initial Commit

---
+++ Shared/ClassesApp.swift
@@ -0,0 +1,20 @@
+//
+//  ClassesApp.swift
+//  Shared
+//
+//  Created by Nick Baughan on 05/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ClassesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
